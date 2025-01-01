540c36d570554531e313e6dc6d3b6ab3811cc0a4
gpetersonrust <ginopetersondevelopments2023@gmail.com>
2025-01-01 15:27:43 -0500

    Mimic Browser in context options

--- includes/admin-pages.php
+++ includes/admin-pages.php
@@ -189,7 +189,9 @@ class ExportImportHealthcareProviders {
         $context_options = [
             'http' => [
                 'method' => 'GET',
-                'header' => 'Accept: application/json',
+                // Mimic browser request and accept JSON response
+                'header' => "Accept: application/json\r\n" .
+                            "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36\r\n"
             ]
         ];
     
